e682784356c539850e281204400aab9d76b0e8fe
Rob Simpson <robertsimpson201291@gmail.com>
2014-08-30 16:56:38 +0100

    Refactored most of the base SCSS

--- index.php
+++ index.php
@@ -10,11 +10,55 @@ include_once('snippets/head.php');
 	<?php include_once('snippets/nav.php'); ?>
 
 	<div class="section-padding--tb section-padding--lr">
-		<div class="wrap">
+		<div class="wrap--site">
 			<main class="main-content">
-				<div class="wrap__inner">
+				<div class="wrap--content">
 					<h1>Index</h1>
-					<a href="https://twitter.com/intent/tweet?url=<?php echo $_SERVER['REQUEST_URI'] ?>">tweet</a>
+
+					<label for="input" class="label">input</label>
+					<input type="text" class="input" id="input" placeholder="label">
+
+					<label for="select" class="label">select</label>
+					<div class="select-wrap">
+						<select name="" class="select" id="select">
+							<option value="option">option</option>
+						</select>
+					</div>
+
+					<label for="textarea" class="label">textarea</label>
+					<textarea name="" class="textarea" id="textarea" cols="30" rows="8"></textarea>
+
+					<input type="radio" class="radio" id="radio1" name="radio">
+					<input type="radio" class="radio" id="radio2" name="radio">
+
+					<input type="checkbox" class="checkbox" id="checkbox1" name="checkbox">
+					<input type="checkbox" class="checkbox" id="checkbox2" name="checkbox">
+
+					<div class="table-responsive">
+						<table class="table">
+							<thead>
+								<tr>
+									<th>heading 1</th>
+									<th>heading 2</th>
+									<th>heading 3</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>something</td>
+									<td>something</td>
+									<td>something</td>
+								</tr>
+								<tr>
+									<td>something</td>
+									<td>something</td>
+									<td>something</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+
+					<abbr title="hyper text markup language">HTML</abbr>
 				</div> <!-- .wrap__inner -->
 			</main>
 		</div> <!-- .wrap -->
