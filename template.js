19cd34817b54b1ff1099d80fff6614446a19bfdb
Rob Simpson <robertsimpson201291@gmail.com>
2015-01-17 19:03:10 +0000

    Automatically run npm & bower install after init

--- template.js
+++ template.js
@@ -1,5 +1,7 @@
 /* global exports: true */
 var fs = require( 'fs' );
+var exec = require( './lib/exec.js' ).exec;
+var Q = require( './lib/q.js' );
 
 exports.description = "Creates the Geckotree front-end boilerplate.";
 
@@ -10,5 +12,10 @@ exports.template = function( grunt, init, done ) {
 		var files = init.filesToCopy( props );
 
 		init.copyAndProcess( files, props );
+
+		return Q.all([
+			exec( 'npm install' ),
+			exec( 'bower install' )
+		]);
 	});
 };
