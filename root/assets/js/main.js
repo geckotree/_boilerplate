01d2b86b9d44c98289a0f3f42ad9370886acade9
Rob Simpson <robertsimpson201291@gmail.com>
2015-01-25 08:52:01 +0000

    Created v1 of carousel.js

--- root/assets/js/main.js
+++ root/assets/js/main.js
@@ -5,17 +5,28 @@ define( function( require ) {
 		'localStorage' in window &&
 		'addEventListener' in window ) {
 
-		var Toggle = require( 'lib/toggle' );
+		var Toggle = require( 'plugins/toggle' );
+		var Carousel = require( 'plugins/carousel' );
+
 		var $html = document.querySelector( 'html' );
 		var $toggle = document.querySelectorAll( '[data-toggle]' );
+		var $carousel = document.querySelectorAll( '.js-carousel' );
 
 		$html.classList.remove( 'no-mustard' );
 		$html.classList.add( 'mustard' );
 
 		if( $toggle.length ) {
 			for( var i in $toggle ) {
-				if( $toggle[i].nodeType == 1 ) {
-					new Toggle( $toggle[i] );
+				if( $toggle[ i ].nodeType == 1 ) {
+					new Toggle( $toggle[ i ] );
+				}
+			}
+		}
+
+		if( $carousel.length ) {
+			for( var x in $carousel ) {
+				if( $carousel[ x ].nodeType == 1 ) {
+					new Carousel( $carousel[ x ] );
 				}
 			}
 		}
