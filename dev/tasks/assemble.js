76211bddba57d6982891453e408015e8388d4c1d
Rob Simpson <robertsimpson201291@gmail.com>
2015-07-04 13:12:59 +0100

    added forms to pattern lab style guide

--- dev/tasks/assemble.js
+++ dev/tasks/assemble.js
@@ -3,7 +3,7 @@ module.exports = {
 	options: {
 		data: 'patterns/**/*.{json,yml}',
 		helpers: 'patterns/helpers/helper-*.js',
-		layout: 'patterns/src/layouts/default.hbs',
+		layout: 'patterns/src/layouts/styleguide.hbs',
 		partials: [ 'patterns/src/components/**/*.hbs' ]
 	},
 	pages: {
